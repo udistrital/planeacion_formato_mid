143c144cfb5481ced0389d6fd9857979dba96586
Steven Espejo <55455142+C0C045@users.noreply.github.com>
2024-07-30 14:06:52 -0500

    feat: auditoria librería

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/udistrital/planeacion_formato_mid/routers"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	xray "github.com/udistrital/utils_oas/xray"
+	auditoria "github.com/udistrital/utils_oas/auditoria"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
@@ -34,5 +35,6 @@ func main() {
 	xray.InitXRay()
 	beego.ErrorController(&customerrorv2.CustomErrorController{})
 	apistatus.Init()
+	auditoria.InitMiddleware()
 	beego.Run()
 }
